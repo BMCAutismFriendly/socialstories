3d4a9ed61f2f05297acca91f26f08dc2703e0303
BMCAutismFriendly <59740404+BMCAutismFriendly@users.noreply.github.com>
2020-01-23 12:23:14 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,10 +5,6 @@
  * and open the template in the editor.
  */
 
-function mainmenu(){
-    window.location.assign("https://bmcautismfriendly.github.io/socialstories/");
-}
-
 function doctorsoffice(){
     window.location.assign("https://bmcautismfriendly.github.io/socialstories/doctorsoffice/");
 }
@@ -21,55 +17,8 @@ function aboutbmcautism(){
     window.location.assign("https://bmcautismfriendly.github.io/socialstories/about-bmc-autism/");
 }
 
-//Writes settings.html form values to str, which is appended to end of URL
-function begin(){
-    var str="";
-	str = str + "&start="+start[start.selectedIndex].value;
-	str = str + "&building="+building[building.selectedIndex].value;
-	str = str + "&department="+department[department.selectedIndex].value;
-	window.location.assign("https://bmcautismfriendly.github.io/socialstories/doctorsoffice/gettingtoBMC.html?pos=0"+str);
-}
-
 function exit(){
 	window.location.assign("https://bmcautismfriendly.github.io/socialstories/");
-}
-
-function settings(){
-    window.location.assign("https://bmcautismfriendly.github.io/socialstories/doctorsoffice/settings/");
-}
-
-function resume(pos, start, building, department){
-    window.location.assign("https://bmcautismfriendly.github.io/socialstories/doctorsoffice/gettingtoBMC.html?"+"pos="+pos+"&start="+start+"&building="+building+"&department="+department);
-}
-
-function previous(pos, start, building, department){
-    var p=0;
-    
-    if (pos===0){
-        window.location.assign("https://bmcautismfriendly.github.io/socialstories/doctorsoffice/settings/");
-    }
-    
-    else{
-        if ((department==="y1" || department==="p0") && (pos===11)){
-            p=5;
-        }
-        
-        else if ((department==="y1" || department==="p0") && (pos===12)){
-            p=7;
-        }
-        
-        else if ((department==="y1" || department==="p0") && (pos===13)){
-            p=7;
-        }
-    
-        else{
-            if (pos===1){p=0;}
-            else if (pos%2===0){p=pos-1;}
-            else{p=pos-2;}
-        }
-    
-        window.location.assign("https://bmcautismfriendly.github.io/socialstories/doctorsoffice/gettingtoBMC.html?"+"pos="+p+"&start="+start+"&building="+building+"&department="+department);
-    }
 }
 
 function medicalprocedures(){
@@ -88,113 +37,9 @@ function blooddraw(){
     window.location.assign("https://bmcautismfriendly.github.io/socialstories/blood-draw/");
 }
 
-function resume1(pos, story){
-    window.location.assign(story+"pos="+pos);
-}
-
-function previous1(pos, story){
-    var p=0;
-    
-    if (pos===0){
-        window.location.assign("https://bmcautismfriendly.github.io/socialstories/medical-procedures/");
-    }
-    
-    else{
-        if (pos===1){p=0;}
-        else if (pos%2===0){p=pos-1;}
-        else{p=pos-2;}
-    
-        window.location.assign(story+"pos="+p);
-    }
-}
-
-function restart1(story){
-    window.location.assign(story+"pos=0");
-}
-
 function heightandweight(){
     window.location.assign("heightandweight.html?pos=0");
 }
-
-function configureDropDownLists(building,department) {
-    var doctorsoffice = ['Psychiatry Research Center'];
-    var dowling = ['Behavioral Health', 'Child Psychiatry'];
-    var familymedicine = ['Family Medicine'];
-    var menino = ['Radiology'];
-    var moakley = ['Audiology', 'Blood Draw Station', 'Hematology', 'Gastroenterology', 'Oncology', 'Otolaryngology'];
-    var preston = ['Blood Draw Station','Cardiovascular Center', 'Center for Endocrinology, Diabetes, Nutrition & Weight Management', 'Physical/Occupational Therapy', 'Renal Medicine', 'TB Clinic'];
-    var shapiro = ['Adult Primary Care', 'Blood Collection Station', 'Dermatology', 'Neurology', 'Pediatric Neurology'];
-    var yawkey = ['Adolescent Medicine', 'Blood Draw Station', 'Dentistry', 'Family Medicine', 'Ophthalmology', 'Pediatric Primary Care', 'Pediatric Dentistry', 'Pediatric Specialty'];
-
-    switch (building.value) {
-        case "doctorsoffice":
-            department.options.length = 0;
-            for (i = 0; i < doctorsoffice.length; i++) {
-                createOption(department, doctorsoffice[i], "do"+i);
-            }
-            break;
-            
-        case "dowling":
-            department.options.length = 0;
-            for (i = 0; i < dowling.length; i++) {
-            	createOption(department, dowling[i], "d"+i);
-            }
-            break;
-
-        case "familymedicine":
-            department.options.length = 0;
-            for (i = 0; i < familymedicine.length; i++) {
-                createOption(department, familymedicine[i], "f"+i);
-            }
-            break;
-            
-        case "menino":
-            department.options.length = 0;
-            for (i = 0; i < menino.length; i++) {
-                createOption(department, menino[i], "m"+i);
-            }
-            break;
-
-        case "moakley":
-            department.options.length = 0;
-            for (i = 0; i < moakley.length; i++) {
-                createOption(department, moakley[i], "mo"+i);
-            }
-            break;
-            
-        case "preston":
-            department.options.length = 0;
-            for (i = 0; i < preston.length; i++) {
-                createOption(department, preston[i], "p"+i);
-            }
-            break;
-        
-        case "shapiro":
-            department.options.length = 0;
-            for (i = 0; i < shapiro.length; i++) {
-                createOption(department, shapiro[i], "s"+i);
-            }
-            break;
-        
-        case "yawkey":
-            department.options.length = 0;
-            for (i = 0; i < yawkey.length; i++) {
-                createOption(department, yawkey[i], "y"+i);
-            }
-            break;
-            
-        default:
-            department.options.length = 0;
-            break;
-        }
-}
-
-    function createOption(department, text, value) {
-        var opt = document.createElement('option');
-        opt.value = value;
-        opt.text = text;
-        department.options.add(opt);
-    }
     
     function physicalexam(){
         window.location.assign("physicalexam.html?pos=0");
