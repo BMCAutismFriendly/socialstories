973fa011c83b44fe7fc653a3cffa87bbbdbe2044
BMCAutismFriendly <59740404+BMCAutismFriendly@users.noreply.github.com>
2021-01-15 16:46:43 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -108,25 +108,3 @@ function period(){
 function inhaler(){
     redir("https://bmcautismfriendly.github.io/socialstories/inhaler/");
 }
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function restart() {
-    redir('https://bmcautismfriendly.github.io/socialstories/wearingglasses/');
-}
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("slide");
-  if (n > x.length) {redir('https://bmcautismfriendly.github.io/socialstories/')}
-  if (n < 1) {redir('https://bmcautismfriendly.github.io/socialstories/')}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
