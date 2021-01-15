cce8e00cd6ba9bfc8858f1b46e22f6d2feb560d0
BMCAutismFriendly <59740404+BMCAutismFriendly@users.noreply.github.com>
2021-01-15 16:33:00 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -108,3 +108,25 @@ function period(){
 function inhaler(){
     redir("https://bmcautismfriendly.github.io/socialstories/inhaler/");
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function restart() {
+    window.location.assign('https://bmcautismfriendly.github.io/socialstories/wearingglasses/');
+}
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  if (n > x.length) {window.location.assign('https://bmcautismfriendly.github.io/socialstories/')}
+  if (n < 1) {window.location.assign('https://bmcautismfriendly.github.io/socialstories/')}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
