42160c6060667e0ad1370c9ebb1b61c301909832
BMCAutismFriendly <59740404+BMCAutismFriendly@users.noreply.github.com>
2021-02-19 12:44:29 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -98,7 +98,7 @@ function takingmedicine(){
 }
 
 function wearingglasses(){
-    redir("https://htmlpreview.github.io/?https://github.com/BMCAutismFriendly/socialstories/blob/multi_lang/test/espanol_asdf.html"); //test; "https://bmcautismfriendly.github.io/socialstories/wearingglasses/"
+    redir("https://bmcautismfriendly.github.io/socialstories/wearingglasses/"); 
 }
 
 function mri(){
